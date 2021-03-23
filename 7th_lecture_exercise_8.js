19b5d7a040f83b45660dbdfc814fd3f2ae19f364
ekfka4863 <ekfka4863@gmail.com>
2021-03-23 23:53:16 +0900

    Commit: Clean Code by 찬경

--- 7th_lecture_exercise_8.js
+++ 7th_lecture_exercise_8.js
@@ -19,4 +19,14 @@ function countCompletedTodos() {
 console.log(countCompletedTodos()); // 1
 
 
-// ------------- Pseudo Code ---------------- //
+// ------------- Clean Code ---------------- //
+let todos = [
+	{ id: 3, content: 'HTML', completed: false },
+	{ id: 2, content: 'CSS', completed: true },
+	{ id: 1, content: 'Javascript', completed: false }
+];
+function countCompletedTodos() {
+		const result = todos.reduce((acc,cur) => cur.completed ? acc+= 1 : acc, 0 );
+		return result;
+}
+console.log(countCompletedTodos()); // 1
