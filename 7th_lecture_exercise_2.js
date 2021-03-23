dcea081b54f6c0aefb05c32546f701cbc059d19e
ekfka4863 <ekfka4863@gmail.com>
2021-03-23 23:51:19 +0900

    Commit: Clean Code by 찬경

--- 7th_lecture_exercise_2.js
+++ 7th_lecture_exercise_2.js
@@ -24,3 +24,17 @@ console.log(getValues('completed'));
 // console.log(getValues('id')); // [3, 2, 1]
 // console.log(getValues('content')); // ['HTML', 'CSS', 'Javascript']
 // console.log(getValues('completed')); // [false, true, false]
+
+// --------------- Clean Code --------------- // 
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+function getValues(key) {
+  const result =  todos.map((todo) => todo[key])
+  return result
+}
+console.log(getValues('id'));
+console.log(getValues('content'));
+console.log(getValues('completed'));
