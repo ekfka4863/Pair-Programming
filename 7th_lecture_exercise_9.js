2868546a224ca759fc8cc5061872c0b5a64276d6
ekfka4863 <ekfka4863@gmail.com>
2021-03-24 11:37:47 +0900

    Commit: Clean Code by 찬경

--- 7th_lecture_exercise_9.js
+++ 7th_lecture_exercise_9.js
@@ -19,4 +19,13 @@ function getMaxId() {
 console.log(getMaxId()); // 3
 
 
-// ------------- Pseudo Code ---------------- //
+// ------------- Clean Code ---------------- //
+const todos = [
+	{ id: 3, content: 'HTML', completed: false },
+	{ id: 2, content: 'CSS', completed: true },
+	{ id: 1, content: 'Javascript', completed: false }
+];
+function getMaxId() {
+	return Math.max(...todos.map( todo => todo.id));   //
+}
+console.log(getMaxId()); // 3
